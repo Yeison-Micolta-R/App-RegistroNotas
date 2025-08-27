fb1f18c2544c823a4a8e05799b7bef4fb99a0b11
Yeison Micolta <ymicolta@udenar.edu.co>
2025-08-27 09:01:14 -0500

    Iniciando el backend de la app agregando dependecias de spring

---
+++ src/main/java/com/example/App_RegNotas/AppRegNotasApplication.java
@@ -0,0 +1,13 @@
+package com.example.App_RegNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRegNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRegNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/App_RegNotas/AppRegNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.App_RegNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRegNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
